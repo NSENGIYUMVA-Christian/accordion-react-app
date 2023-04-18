368c4719b595bcc6575d1c97019ca5e45e5f7554
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-18 17:11:00 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "Who is NSENGIYUMVA CHRISTIAN",
+    info: "Christian is an aspiring to become a software developer, his passion lies in creating innovative solutions through coding. The idea of designing, developing, and implementing software programs that can simplify and enhance people's lives excites him. he is constantly learning and expanding his skills in various programming languages to stay up-to-date with the latest technologies. To him, software development is not just a career choice, but a journey of continuous learning and growth",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+  },
+];
+export default questions;
